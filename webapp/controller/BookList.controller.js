f7e15905e68999c9bae83a71399ba0e745b36728
Petrean Simona <50493944+petreansimona@users.noreply.github.com>
2021-06-09 22:16:03 +0300

    versiune finala

--- webapp/controller/BookList.controller.js
+++ webapp/controller/BookList.controller.js
@@ -5,141 +5,78 @@ sap.ui.define([
 	'sap/m/MessageToast',
 	"sap/m/MessageBox",
 	"sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator"
-], function (Controller, JSONModel, formatter,MessageToast, MessageBox, Filter, FilterOperator) {
+	"sap/ui/model/FilterOperator",
+	'sap/ui/model/odata/v2/ODataModel',
+	'sap/ui/core/util/MockServer'
+], function (Controller, JSONModel, formatter,MessageToast, MessageBox, Filter, FilterOperator, ODataModel, MockServer) {
 	"use strict";
 	return Controller.extend("Quickstart.controller.BookList", {
 		formatter: formatter,
 
 		handleDelete: function(oEvent){
-			var sBookPath=oEvent.getParameter('listItem').getBindingContext().getPath();
-			this.getView().getModel().remove(sBookPath,{
-				success: function(){
-					MessageToast.show("Book deleted!");
-				}
-			})
-		},//////
 
+			var oEntry = {};
+			oEntry.ISBN = this.byId("ISBN").getValue();
+
+			this.getView().getModel().remove("/BooksSet('"+oEntry.ISBN+"')",{
+				success: function() {
+					MessageToast.show("Book removed!");
+				},
+				error: function(){
+					MessageToast.show("Book was not removed!");
+				}
+			});
+		},
 		handleUpdate: function(oEvent){
-			// var sBookPath=oEvent.getParameter('listItem').getBindingContext().getPath();
-			// this.getView().getModel().update(sBookPath,{
-			// 	success: function(){
-			// 		MessageToast.show("Book updated!");
-			// 	}
-			// })
 			
 			var oEntry = {};
-			oEntry.ISBN = $CreateTable1;
-			oEntry.Title = $CreateTable2;
-			oEntry.Author = $CreateTable3;
-			oEntry.DatePublished = $CreateTable4;
-			oEntry.Language = $CreateTable5;
-			oEntry.TotalNrBooks = $CreateTable6;
-			oEntry.NrAvailable = $CreateTable7;
-			oEntry.CreatedOn = $CreateTable8;
-			oEntry.CreatedBy = $CreateTable9;
-			oEntry.ChangedOn = $CreateTable10;
-			oEntry.ChangedBy = $CreateTable11;
+			oEntry.ISBN = this.byId("ISBN").getValue();
+			oEntry.Title = this.byId("Title").getValue();
+			oEntry.Author = this.byId("Author").getValue();
+			oEntry.DatePublished = this.byId("Datepublished").getDateValue();
+			oEntry.Language = this.byId("Language").getValue();
+			oEntry.TotalNrBooks = parseInt(this.byId("TotalNrBooks").getValue());
+			oEntry.NrAvailable = parseInt(this.byId("NrAvailable").getValue());
+			oEntry.CreatedOn = "";
+			oEntry.CreatedBy = "";
+			oEntry.ChangedOn = "";
+			oEntry.ChangedBy = "";
 
-			var oData = new Quickstart.ODataModel("/sap/opu/odata/sap/ZBOOKS_SIPE_SRV");
-
-			this.oData.update("/BooksSet(ISBN='"+oEntry.ISBN+"')",oEntry,null,function(){
-				MessageToast.show("Book updated!");
-			},function(){
-				alert("Error!");
+			this.getView().getModel().update("/BooksSet('"+oEntry.ISBN+"')",oEntry,{
+				success: function() {
+					MessageToast.show("Book updated!");
+				},
+				error: function(){
+					MessageToast.show("Book was not updated!");
+				}
 			});
+
 
 		},
 
 		handleCreate: function(oEvent){
-			// var sBookPath=oEvent.getParameter('listItem').getBindingContext().getPath();
-			// this.getView().getModel().create(sBookPath,{
-			// 	success: function(){
-			// 		MessageToast.show("Book created!");
-			// 	}
-			// });
 
 			var oEntry = {};
-			oEntry.ISBN = $CreateTable1;
-			oEntry.Title = $CreateTable2;
-			oEntry.Author = $CreateTable3;
-			oEntry.DatePublished = $CreateTable4;
-			oEntry.Language = $CreateTable5;
-			oEntry.TotalNrBooks = $CreateTable6;
-			oEntry.NrAvailable = $CreateTable7;
-			oEntry.CreatedOn = $CreateTable8;
-			oEntry.CreatedBy = $CreateTable9;
-			oEntry.ChangedOn = $CreateTable10;
-			oEntry.ChangedBy = $CreateTable11;
+			oEntry.ISBN = this.byId("ISBN").getValue();
+			oEntry.Title = this.byId("Title").getValue();
+			oEntry.Author = this.byId("Author").getValue();
+			oEntry.DatePublished = this.byId("Datepublished").getDateValue();
+			oEntry.Language = this.byId("Language").getValue();
+			oEntry.TotalNrBooks = parseInt(this.byId("TotalNrBooks").getValue());
+			oEntry.NrAvailable = parseInt(this.byId("NrAvailable").getValue());
+			oEntry.CreatedOn = "";
+			oEntry.CreatedBy = "";
+			oEntry.ChangedOn = "";
+			oEntry.ChangedBy = "";
 
-			var oData = new Quickstart.ODataModel("/sap/opu/odata/sap/ZBOOKS_SIPE_SRV");
-
-			this.oData.create('/BooksSet',oEntry,null,function(){
-				MessageToast.show("Book created!");
-			}, function(){
-				alert("Error!");
-			})
-		},
-
-		onFilterISBN : function (oEvent) {
-
-			// build filter array
-			// var aFilter = [];
-			// var sQuery = oEvent.getParameter("query");
-			// if (sQuery) {
-			// 	aFilter.push(new Filter("ISBN", FilterOperator.Contains, sQuery));
-			// }
-
-			// filter binding
-			// var oList = this.byId("bookList");
-			// var oBinding = oList.getBinding("items");
-			// oBinding.filter(aFilter);
-
-			var sQuery = oEvent.getSource().getValue();  
-    
-    		var oFilter = new sap.ui.model.Filter({
-    
-			filters: [
-
-				new sap.ui.model.Filter("ISBN", sap.ui.model.FilterOperator.Contains, sQuery)
-				//new sap.ui.model.Filter("value2", sap.ui.model.FilterOperator.Contains, sQuery)
-
-			],
-			
-			and: false
-			
+			this.getView().getModel().create("/BooksSet",oEntry,{
+				success: function() {
+					MessageToast.show("Book saved!");
+				},
+				error: function(){
+					MessageToast.show("Book was not saved!");
+				}
 			});
-			var oBinding = this.byId("bookList").getBinding("items");     
-         
-    		oBinding.filter([oFilter]);
-		},
-		// onCreate : function () {
-		// 	var oList = this.byId("listItem"),
-		// 		oBinding = oList.getBinding("items"),
-		// 		oContext = oBinding.create({
-		// 		"ISBN" : "",
-		// 		"Title" : "",
-		// 		"Author" : "",
-		// 		"Date Published" : "",
-		// 		"Language" : "",
-		// 		"Total Nr Books" : "",
-		// 		"Nr Available" : "",
-		// 		"Created On" : "",
-		// 		"Created By" : "",
-		// 		"Changed On" : "",
-		// 		"Changed By" : ""
-		// 		});
-
-		// 	//this._setUIChanges();
-		// 	this.getView().getModel("appView").setProperty("/usernameEmpty", true);
-
-		// 	oList.getItems().some(function (oItem) {
-		// 		if (oItem.getBindingContext() === oContext) {
-		// 			oItem.focus();
-		// 			oItem.setSelected(true);
-		// 			return true;
-		// 		}
-		// 	});
-		// },
+		}
 	});
 });
